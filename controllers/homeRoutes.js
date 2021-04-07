b1458835da2e226da4615d974ed417cb6e118ad6
Richard Seward <raseward14@gmail.com>
2021-04-06 20:58:07 -0600

    render login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,80 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+// render homepage
+router.get('/', withAuth, async (req, res) => {
+    try {
+        // Get all posts and JOIN with user data
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] },
+            order: [['name', 'ASC']],
+          });
+        // Serialize data so the template can read it
+        const users = userData.map((post) => post.get(
+            { plain: true }
+        ));
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            users,
+            logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
+// render a post
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                }
+            ]
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// render profile, use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        // find logged in user based on session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }]
+        });
+        // serialize data so template can read it
+        const user = userData.get({ plain: true });
+        // pass serialized data and session flag into template
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// render login
+router.get('/login', async (req, res) => {
+    // if user already logged in, redirect request to another route
+    if (req.session.logged_in) {
+        res.redirect('profile');
+        return;
+    }
+    res.render('login');
+});
 
 module.exports = router;
