16dfa069ed18fd4192ed0a535f7510b83bda0fa4
Richard Seward <raseward14@gmail.com>
2021-04-11 16:21:33 -0600

    navbar logout renders only when logged in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -21,6 +21,7 @@ router.get('/', async (req, res) => {
         // Pass serialized data and session flag into template
         res.render('homepage', {
             posts,
+            logged_in: req.session.logged_in
         })
     } catch (err) {
         res.status(500).json(err)
