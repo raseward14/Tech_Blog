a3f1196cc97ea2a7ddd110e2e78e1b7e2b16e494
Richard Seward <raseward14@gmail.com>
2021-04-11 15:48:59 -0600

    created db comments model & seeded model

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -13,6 +15,8 @@ const seedDatabase = async () => {
   });
 
   const posts = await Post.bulkCreate(postData);
+
+  const comments = await Comment.bulkCreate(commentData);
 
   process.exit(0);
 };
